b00d398f9c881fe132547ba1674da76bfe817f2d
Sagi Shadur <saroad2@gmail.com>
2021-05-18 14:22:13 +0300

    Replace up and down

--- src/snake_learner/direction.py
+++ src/snake_learner/direction.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 TO_ARRAY = {
-    "UP": np.array([-1, 0]),
-    "DOWN": np.array([1, 0]),
+    "UP": np.array([1, 0]),
+    "DOWN": np.array([-1, 0]),
     "RIGHT": np.array([0, 1]),
     "LEFT": np.array([0, -1]),
 }
